cfa20ff31e5e6b016c4ca1afc8daad13812c8c30
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-03-04 14:55:02 +0700

    Casting variable in Python

---
+++ Casting_Variable.py
@@ -0,0 +1,13 @@
+#Casting variable in Python.
+
+x = float(5) #It is Integer by default
+y = float("3") #It is String by default
+a = float(20) #It is Integer by default
+b = float("5.5") # It is String by default
+
+#Convert them in to Float literal.
+print(x)
+print(y)
+print(a)
+print(b)
+#Constructs a float number from Integer and String literal.
