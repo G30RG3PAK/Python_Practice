00cdaaee20f8ff43bab6595976bbcc8402739338
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-03-16 22:42:50 +0700

    Return value(return statement)

---
+++ Return_Value.py
@@ -0,0 +1,9 @@
+#Return Values
+#return statement : let function return a value
+def re_function(x):
+	return 10 * x
+
+print(re_function(5))
+print(re_function(8))
+print(re_function(12))
+print(re_function(25))
