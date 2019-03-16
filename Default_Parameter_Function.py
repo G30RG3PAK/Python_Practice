dc82e6ef46e67ec3b9929f6e33a86d4633c927c8
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-03-16 22:36:07 +0700

    Call function without parameter

---
+++ Default_Parameter_Function.py
@@ -0,0 +1,13 @@
+#Default Parameter Value
+#Call function without parameter
+
+def nation_function(country = "Norway"):
+	print("I am form" +' '+country)
+
+nation_function("Denmark")
+
+nation_function("Sweden")
+
+nation_function()
+
+nation_function("German")
