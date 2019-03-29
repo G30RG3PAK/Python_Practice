e5b0bcc60c305d7e6e3c248330c914e075da7271
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-03-29 17:08:48 +0700

    Update Inheritance_pass_function.py

--- Inheritance_pass_function.py
+++ Inheritance_pass_function.py
@@ -9,7 +9,7 @@ class Person:
 
 class Student(Person):
 	pass
-
+#Use the pass keyword when you do not want to add any other properties or methods to the class.
 x = Student("Domminic", "Tomato")
 x.showname()
  
