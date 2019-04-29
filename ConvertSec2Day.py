8c63903ec4937f4b6ccee3d945e12ad28e4f3183
Pakpoom Rachtrachoo <33973448+G30RG3PAK@users.noreply.github.com>
2019-04-29 18:54:45 +0700

    Convert Second(input) to D/H/M/S

---
+++ ConvertSec2Day.py
@@ -0,0 +1,10 @@
+time = float(input("Input time in second: "))
+day = time // (24 * 3600)
+time = time % (24 * 3600)
+hour = time // 3600
+time %= 3600
+minutes = time // 60
+time %= 60
+second = time
+print("Day:Hour:Minutes:Second --> %d:%d:%d:%d" % (day, hour, minutes, second))
+
